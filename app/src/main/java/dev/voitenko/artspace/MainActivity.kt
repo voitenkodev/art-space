90810a52c7169aa12093821036b38a8ce6697c3c
maxvoitenko <voitenko.dev@gmail.com>
2022-08-19 21:44:39 +0300

    init

---
+++ app/src/main/java/dev/voitenko/artspace/MainActivity.kt
@@ -0,0 +1,17 @@
+package dev.voitenko.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.view.WindowCompat
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // This app draws behind the system bars, so we want to handle fitting system windows
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+
+        }
+    }
+}
